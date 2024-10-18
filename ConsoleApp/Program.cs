b947140908debefb6d8fdbd95e9b601303efa223
V.Pavlov <v.pavlov@carx-tech.com>
2024-10-18 16:41:34 +0300

    init

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        var result = Console.ReadLine();
+        Console.WriteLine(result);
+    }
+}
